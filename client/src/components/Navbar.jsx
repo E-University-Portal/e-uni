b30322003852e342f0a44d22cb27ed3d1b8dbe5c
VANSHKAKKAR04 <125470711+VANSHKAKKAR04@users.noreply.github.com>
2024-06-11 21:01:54 +0530

    NPTEL upload page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,48 +1,6 @@
-// import { Link } from "react-router-dom";
-// import NavLinks from "./NavLinks";
-// import { useState } from "react";
-// export default function Navbar() {
-//   const [collapsed, setCollapsed] = useState(true);
-
-//   const handleToggle = () => {
-//     setCollapsed(!collapsed);
-//   };
-
-//   return (
-//     <>
-//       <div
-//         className={`${
-//           collapsed ? "w-[5dvw]" : "w-[15dvw]"
-//         } bg-[#73A8FF]  h-full fixed top-0 left-0 flex flex-col items-center justify-between tr
-//         ansition-width duration-300`}
-//       >
-//         <div className="w-full">
-//           <Link
-//             to="/"
-//             className="h-fit flex justify-center items-center mt-4 mb-6"
-//           >
-//             <img src="/logo-black.svg" className="h-[2.5dvw]" />
-//           </Link>
-//           <NavLinks />
-//         </div>
-//         <div
-//           to="/"
-//           className="h-fit flex justify-center rounded-full items-center my-4"
-//         >
-//           <Link to="/profile">
-//             <img src="/profile.svg" className="w-[2.5dvw]" />
-//           </Link>
-//         </div>
-//       </div>
-//       <div className="w-full flex md:hidden h-[7.5dvh] bg-[#73A8FF] fixed bottom-0 left-0">
-//         Wink
-//       </div>
-//     </>
-//   );
-// }
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { LayoutDashboard, ArrowLeftToLine } from "lucide-react";
+import { LayoutDashboard, ArrowLeftToLine, FileBox } from "lucide-react";
 
 export default function Navbar() {
   const [isCollapsed, setIsCollapsed] = useState(false);
@@ -54,69 +12,61 @@ export default function Navbar() {
   return (
     <>
       <div
-        className={`h-full fixed left-0 top-0 bg-primary flex flex-col items-center justify-between transition-all duration-150 ${
-          isCollapsed ? "w-[5dvw]" : "w-[15dvw]"
+        className={`fixed top-0 left-0 bg-primary h-full flex flex-col justify-between transition-all duration-150 ${
+          isCollapsed ? "w-[5dvw]" : "w-[12dvw]"
         }`}
       >
-        <Link to="/" className="h-fit flex justify-center items-center">
+        <Link to="/" className="flex justify-center items-center h-fit">
           <img
             src="/logo-white.svg"
-            className={`transition-transform duration-200 ${
+            className={`${
               isCollapsed
-                ? "w-10 h-10 ml-5 mt-2"
-                : "h-[3dvw] mt-2 mb-1 ml-8 mr-8"
+                ? "w-5 h-5 ml-5 mr-5 mt-2 transition-transform duration-200"
+                : "w-[5dvw] h-[5dvw] mt-2 mb-1 ml-8 mr-8 transition-transform duration-200"
             }`}
           />
         </Link>
 
-        <div className="w-full flex flex-col items-center space-y-6">
-          <div>
-            <Link
-              to="/dashboard"
-              className="w-full flex flex-col items-center justify-center h-[5dvw] text-white hover:bg-blue-700 rounded-md transition-colors duration-200"
-            >
-              <LayoutDashboard
-                className={`transition-transform duration-200 ${
-                  isCollapsed
-                    ? "w-10 h-10 ml-5 mr-5"
-                    : "w-[3dvw] h-[3dvw] mb-1 ml-8 mr-8"
-                }`}
-              />
-              {!isCollapsed && (
-                <span className="text-md ml-7 mr-7">Dashboard</span>
-              )}
-            </Link>
-          </div>
+        <div className="w-full flex flex-col items-center space-y-4">
+          <Link
+            to="/dashboard"
+            className="flex flex-row space-x-2 items-center justify-center w-full h-[5dvw] text-white hover:bg-blue-700 rounded-md transition-colors duration-200"
+          >
+            <LayoutDashboard
+              color="white"
+              className={`${
+                isCollapsed
+                  ? "w-5 h-5 ml-5 mr-5 transition-transform duration-200"
+                  : "h-[2dvw] w-[2dvw] transition-transform duration-200"
+              }`}
+            />
+            {!isCollapsed && <span className=" ml-4 mr-0 text-md">Dashboard</span>}
+          </Link>
 
-          <div>
-            <Link
-              to="/nptel"
-              className="w-full flex flex-col items-center justify-center h-[5dvw] text-white hover:bg-blue-700  rounded-md transition-colors duration-200"
-            >
-              <img
-                src="/nptel-logo.svg"
-                className={`transition-transform duration-200 ${
-                  isCollapsed
-                    ? "w-10 h-10 ml-5 mr-5"
-                    : "w-[3dvw] h-[3dvw] mb-1 ml-8 mr-8"
-                }`}
-              />
-              {!isCollapsed && <span className="text-md ml-7 mr-7">NPTEL</span>}
-            </Link>
-          </div>
+          <Link
+            to="/nptel"
+            className="flex flex-row space-x-2 items-center justify-center w-full h-[5dvw] text-white hover:bg-blue-700 rounded-md transition-colors duration-200"
+          >
+            <FileBox
+              color="white"
+              className={`${
+                isCollapsed
+                  ? "w-5 h-5 ml-5 mr-5 transition-transform duration-200"
+                  : "h-[2dvw] w-[2dvw] ml-0 mr-6 transition-transform duration-200"
+              }`}
+            />
+            {!isCollapsed && <span className=" ml-2 mr-0 text-md">NPTEL</span>}
+          </Link>
         </div>
 
         <div className="w-full pt-4 mt-6 flex justify-center items-center">
-          <Link
-            to="/profile"
-            className="h-fit flex justify-center items-center"
-          >
+          <Link to="/profile" className="flex justify-center items-center h-fit">
             <img
               src="/profile.svg"
-              className={`transition-transform duration-200 ${
+              className={`${
                 isCollapsed
-                  ? "w-10 h-10 ml-5 mr-5"
-                  : "w-[3dvw] h-[3dvw] mb-1 ml-8 mr-8"
+                  ? "w-5 h-5 ml-5 mr-5 transition-transform duration-200"
+                  : "w-[3dvw] h-[3dvw] mb-1 ml-8 mr-8 transition-transform duration-200"
               }`}
             />
           </Link>
@@ -128,7 +78,10 @@ export default function Navbar() {
             className="text-white p-2 focus:outline-none"
           >
             <ArrowLeftToLine
-              className={`w-6 h-6 duration-200 transition-transform ${isCollapsed && "rotate-180"}`}
+              color="white"
+              className={`w-4 h-4 duration-200 transition-transform ${
+                isCollapsed && "rotate-180"
+              }`}
             />
           </button>
         </div>
